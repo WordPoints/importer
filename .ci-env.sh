81c536ea5e8f8ae3c5e2ec0e289dc6de9d5538d1
JDGrimes <jdg@codesymphony.co>
2015-02-24 17:20:47 -0500

    Remove extra space

--- .ci-env.sh
+++ .ci-env.sh
@@ -4,7 +4,7 @@ set -e
 shopt -s expand_aliases
 
 # Install CubePoints when running tests.
-if [[ $TRAVISCI_RUN == phpunit  ]]; then
+if [[ $TRAVISCI_RUN == phpunit ]]; then
 
 	mkdir -p /tmp/cubepoints
 	curl -s https://downloads.wordpress.org/plugin/cubepoints.3.2.1.zip > /tmp/cubepoints.zip
