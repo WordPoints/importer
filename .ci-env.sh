5a1c40a25980c8f7c994a9d2842b26a78e110227
JDGrimes <jdg@codesymphony.co>
2015-02-27 17:54:14 -0500

    Travis: install cubeponts to the plugins directory
    See #3

--- .ci-env.sh
+++ .ci-env.sh
@@ -6,12 +6,9 @@ shopt -s expand_aliases
 # Install CubePoints when running tests.
 install-cubepoints() {
 
-	mkdir -p /tmp/cubepoints
-	curl -s https://downloads.wordpress.org/plugin/cubepoints.3.2.1.zip > /tmp/cubepoints.zip
-	unzip /tmp/cubepoints.zip -d /tmp/
-
 	mkdir -p /tmp/wordpress/wp-content/plugins/cubepoints
-	ln -s /tmp/cubepoints/ /tmp/wordpress/wp-content/plugins/cubepoints
+	curl -s https://downloads.wordpress.org/plugin/cubepoints.3.2.1.zip > /tmp/cubepoints.zip
+	unzip /tmp/cubepoints.zip -d /tmp/wordpress/wp-content/plugins/
 }
 
 alias setup-phpunit="\setup-phpunit; install-cubepoints"
