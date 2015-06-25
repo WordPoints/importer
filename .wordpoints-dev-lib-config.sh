7f6a6c1913624b73ffeeffd883c052caf00dd6ca
JDGrimes <jdg@codesymphony.co>
2015-06-25 10:28:28 -0400

    Travis CI: Fix plugin dir path

--- .wordpoints-dev-lib-config.sh
+++ .wordpoints-dev-lib-config.sh
@@ -6,9 +6,9 @@ shopt -s expand_aliases
 # Install CubePoints when running tests.
 install-cubepoints() {
 
-	mkdir -p /tmp/wordpress/wp-content/plugins/cubepoints
+	mkdir -p /tmp/wordpress/src/wp-content/plugins/cubepoints
 	curl -s https://downloads.wordpress.org/plugin/cubepoints.3.2.1.zip > /tmp/cubepoints.zip
-	unzip /tmp/cubepoints.zip -d /tmp/wordpress/wp-content/plugins/
+	unzip /tmp/cubepoints.zip -d /tmp/wordpress/src/wp-content/plugins/
 }
 
 # Sets up custom configuration.
